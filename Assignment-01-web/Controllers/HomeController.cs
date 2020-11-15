449426fe45c337ad435fdad17da3f607e796a68b
rasheenruwisha <A00229247@mycambrian.ca>
2020-11-16 02:35:28 +0530

    New: Added completed assignment

---
+++ Assignment-01-web/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Assignment_01_web.Models;
+
+namespace Assignment_01_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult Requests()
+        {
+            ViewBag.Accept = Request.Headers["Accept"];
+            ViewBag.AcceptEncoding = Request.Headers["Accept-Encoding"];
+            ViewBag.UserAgent = Request.Headers["User-Agent"];
+            ViewBag.Host = Request.Headers["Host"];
+            return View("RequestHeaders");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Assignment-01-web/Controllers/LoopController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Assignment_01_web.Controllers
+{
+    public class LoopController : Controller
+    {
+
+       
+        
+        // GET: /<controller>/
+        public IActionResult Loop()
+        {
+         List<string> loopItems = new List<String>();
+            loopItems.Add("Controller Loop item 1");
+            loopItems.Add("Controller Loop item 2");
+            loopItems.Add("Controller Loop item 3");
+            loopItems.Add("Controller Loop item 4");
+            loopItems.Add("Controller Loop item 5");
+            loopItems.Add("Controller Loop item 6");
+            ViewBag.controllerLoopItems = loopItems;
+            return View();
+        }
+
+
+    }
+}
